d52a510471563f5e57c1e74945e532f813e902b1
rennersantana <rennersantana@hotmail.com>
2021-05-14 01:36:39 +0100

    A few editing with css to fix the cards line up

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-function _id(name){
+function _id(name) {
   return document.getElementById(name);
 }
-function _class(name){
+function _class(name) {
   return document.getElementsByClassName(name);
 }
-_class("toggle-password")[0].addEventListener("click",function(){
+_class("toggle-password")[0].addEventListener("click", function () {
   _class("toggle-password")[0].classList.toggle("active");
-  if(_id("password-field").getAttribute("type") == "password"){
-    _id("password-field").setAttribute("type","text");
+  if (_id("password-field").getAttribute("type") == "password") {
+    _id("password-field").setAttribute("type", "text");
   } else {
-    _id("password-field").setAttribute("type","password");
+    _id("password-field").setAttribute("type", "password");
   }
 });
 
-_id("password-field").addEventListener("focus",function(){
+_id("password-field").addEventListener("focus", function () {
   _class("password-policies")[0].classList.add("active");
 });
-_id("password-field").addEventListener("blur",function(){
+_id("password-field").addEventListener("blur", function () {
   _class("password-policies")[0].classList.remove("active");
 });
 
-_id("password-field").addEventListener("keyup",function(){
+_id("password-field").addEventListener("keyup", function () {
   let password = _id("password-field").value;
-  
-  if(/[A-Z]/.test(password)){
+
+  if (/[A-Z]/.test(password)) {
     _class("policy-uppercase")[0].classList.add("active");
   } else {
     _class("policy-uppercase")[0].classList.remove("active");
   }
-  
-  if(/[0-9]/.test(password)){
+
+  if (/[0-9]/.test(password)) {
     _class("policy-number")[0].classList.add("active");
   } else {
     _class("policy-number")[0].classList.remove("active");
   }
-  
-  if(/[^A-Za-z0-9]/.test(password)){
+
+  if (/[^A-Za-z0-9]/.test(password)) {
     _class("policy-special")[0].classList.add("active");
   } else {
     _class("policy-special")[0].classList.remove("active");
   }
-  
-  if(password.length > 7){
+
+  if (password.length > 7) {
     _class("policy-length")[0].classList.add("active");
   } else {
     _class("policy-length")[0].classList.remove("active");
@@ -59,24 +59,24 @@ function getData() {
 
   //Get API
   fetch('https://randomuser.me/api/?results=5')
-      .then(res => res.json())
-      .then(data => {
-          // console.log(data);
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
 
-          let author = data.results;
-          console.log(author);
+      let author = data.results;
+      console.log(author);
 
-          //Get Data Value
-          let output = "";
+      //Get Data Value
+      let output = "";
 
-          //Get Data Loop Through
-          author.forEach(function (lists) {
-              output += `
+      //Get Data Loop Through
+      author.forEach(function (lists) {
+        output += `
               <div class="container">
                   <div class="card mt-4 bg-light">
                       <ul class="list-group">
                           <li class="list-group-item"><h2>Name: ${lists.name.first}</h2></li>
-                          <li class="list-group-item"><img src="${lists.picture.large}"></li>
+                          <li class="list-group-item"><img src="${lists.picture.medium}"></li>
                           <li class="list-group-item">Phone Number: ${lists.cell}</li>
                           <li class="list-group-item">DOB: ${lists.dob.date}</li>
                           <li class="list-group-item">Age: ${lists.dob.age}</li>
@@ -87,25 +87,25 @@ function getData() {
                           <li class="list-group-item">PostCode: ${lists.location.postcode}</li>
                       </ul>
                   </div>
-              </div> `;
-          });
-
-          //Show On Our Screen All Data
-          document.getElementById('output').innerHTML = output;
-
+            </div `;
       });
+
+      //Show On Our Screen All Data
+      document.getElementById('output').innerHTML = output;
+
+    });
 };
 
 //menu itens code
 
 new Vue({
   el: '#app',
-  mounted: function(){$('.collapsible').collapsible();},
+  mounted: function () { $('.collapsible').collapsible(); },
   data: {
     cards: [
-      { title: 'Macaron', src: 'https://res.cloudinary.com/landry-bete/image/upload/v1488769144/dessert14_trnhnj.jpg', description:'Is this thing French ?'},
-     { title: 'Tajine', src: 'https://res.cloudinary.com/landry-bete/image/upload/v1525135352/tajine_mfnbd8.jpg', description:'Moroccan people seem to love that dish...'},
-      { title: 'Cake', src: 'https://res.cloudinary.com/landry-bete/image/upload/v1525135530/cake_lnh2hn.jpg', description:'Eat healthy my boy!'},
+      { title: 'Macaron', src: 'https://res.cloudinary.com/landry-bete/image/upload/v1488769144/dessert14_trnhnj.jpg', description: 'Is this thing French ?' },
+      { title: 'Tajine', src: 'https://res.cloudinary.com/landry-bete/image/upload/v1525135352/tajine_mfnbd8.jpg', description: 'Moroccan people seem to love that dish...' },
+      { title: 'Cake', src: 'https://res.cloudinary.com/landry-bete/image/upload/v1525135530/cake_lnh2hn.jpg', description: 'Eat healthy my boy!' },
     ]
   }
-  })
+})
