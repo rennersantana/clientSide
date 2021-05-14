f924c705c62abc91d7b9f4cca03359aeaaa26955
rennersantana <rennersantana@hotmail.com>
2021-05-14 19:07:42 +0100

    Code formating and css code

--- main.js
+++ main.js
@@ -78,13 +78,7 @@ function getData() {
                           <li class="list-group-item"><h2>Name: ${lists.name.first}</h2></li>
                           <li class="list-group-item"><img src="${lists.picture.medium}"></li>
                           <li class="list-group-item">Phone Number: ${lists.cell}</li>
-                          <li class="list-group-item">DOB: ${lists.dob.date}</li>
-                          <li class="list-group-item">Age: ${lists.dob.age}</li>
-                          <li class="list-group-item">Email ID: ${lists.email}</li>
-                          <li class="list-group-item">Gender: ${lists.gender}</li>
-                          <li class="list-group-item">City: ${lists.location.city}</li>
-                          <li class="list-group-item">Country: ${lists.location.country}</li>
-                          <li class="list-group-item">PostCode: ${lists.location.postcode}</li>
+                         
                       </ul>
                   </div>
             </div `;
@@ -109,3 +103,4 @@ new Vue({
     ]
   }
 })
+
