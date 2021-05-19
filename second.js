e199da56b21895ba934fddb3d1921ffba2504a05
rennersantana <rennersantana@hotmail.com>
2021-05-19 20:07:53 +0100

    Made some editions to get a nice looking

--- second.js
+++ second.js
@@ -6,12 +6,12 @@ var rotateValue = circle.style.transform;
 var rotateSum;
 
 upBtn.onclick = function() {
-    rotateSum = rotateValue + "rotate(-90deg";
+    rotateSum = rotateValue + "rotate(-90deg)";
     circle.style.transform = rotateSum;
     rotateValue = rotateSum;
 }
 downBtn.onclick = function() {
-    rotateSum = rotateValue + "rotate(90deg";
+    rotateSum = rotateValue + "rotate(90deg)";
     circle.style.transform = rotateSum;
     rotateValue = rotateSum;
 }
